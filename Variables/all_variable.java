640260e10a3d13e79c22fde5bcd8bed0a366c3b0
Anjali-Kumari-max~ <anjalirakshi@gmail.com>
2025-06-10 10:04:24 +0530

    create a file on variables

---
+++ Variables/all_variable.java
@@ -0,0 +1,34 @@
+package Variables;
+
+class all_variable{
+    public static void main(String[] args) {
+        int integer_variable;
+        byte a;
+        short B;
+        long c;
+        float d;
+        double f;
+        char l;
+        boolean m;
+
+        // initialization
+        integer_variable=100;
+        a=127;
+        B=1;
+        c=0;
+        d=5.0f;
+        f=9.885585757555798598978797;
+        l=6500;
+        m=true;
+
+        // print values
+        System.out.println(integer_variable);
+        System.out.println(a);
+        System.out.println(B);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(m);
+    }
+}
